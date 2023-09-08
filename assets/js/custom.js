8159dfd37e781c2bfbc0ca620cb3e8f5219ac292
Wai Linn Kyaw <stephensuu03@gmail.com>
2023-09-09 00:09:08 +0630

    a

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -96,7 +96,7 @@ $('.responsive1').slick({
     dots: false,
     infinite: true,
     speed: 300,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     prevArrow: '<i class="fas fa-angle-left left_arrow1 arrow">',
@@ -105,10 +105,10 @@ $('.responsive1').slick({
         {
         breakpoint: 1024,
         settings: {
-            slidesToShow: 2,
+            slidesToShow: 1,
             slidesToScroll: 1,
             infinite: true,
-            dots: true
+            dots: false
         }
         },
         {
@@ -145,10 +145,10 @@ $('.responsive2').slick({
         {
         breakpoint: 1024,
         settings: {
-            slidesToShow: 2,
+            slidesToShow: 1,
             slidesToScroll: 1,
             infinite: true,
-            dots: true
+            dots: false
         }
         },
         {
